1dde85ce2abe165cfa758ed7c0b43f7cb0ba224b
strohli <libor@f13cybertech.com>
2021-01-30 14:16:06 +0100

    login

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-import kivy
 from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+
+
+class Login(GridLayout):
+
+    def __init__(self, **kwargs):
+        super(Login, self).__init__(**kwargs)
+        self.cols = 2
+        self.add_widget(Label(text='User Name'))
+        self.username = TextInput(multiline=False)
+        self.add_widget(self.username)
+        self.add_widget(Label(text='Password'))
+        self.password = TextInput(password=True, multiline=False)
+        self.add_widget(self.password)
+
 
 class MyApp(App):
-    def build(self):
-        return Label(text="asdasd")
 
-if __name__ == "__main__":
+    def build(self):
+        return Login()
+
+
+if __name__ == '__main__':
     MyApp().run()
