22782683cc53ad37226dbc4b37acc16d41d1b090
strohli <libor@f13cybertech.com>
2021-01-30 14:25:35 +0100

    vytvořeno menu

--- main.py
+++ main.py
@@ -2,27 +2,33 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 
+Builder.load_string("""
+<MenuScreen>:
+    BoxLayout:
+        Button:
+            text: 'Goto login'
+            on_press: root.manager.current = 'login'
+        Button:
+            text: 'Quit'
+""")
 
-class Login(GridLayout):
+class MenuScreen(Screen):
+    pass
 
-    def __init__(self, **kwargs):
-        super(Login, self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text='User Name'))
-        self.username = TextInput(multiline=False)
-        self.add_widget(self.username)
-        self.add_widget(Label(text='Password'))
-        self.password = TextInput(password=True, multiline=False)
-        self.add_widget(self.password)
+class Login(Screen):
+    pass
 
 
 class MyApp(App):
 
     def build(self):
-        return Login()
-
-
+        sm = ScreenManager()
+        sm.add_widget(MenuScreen(name='menu'))
+        sm.add_widget(Login(name='login'))
+        return sm
 
 if __name__ == '__main__':
     MyApp().run()
