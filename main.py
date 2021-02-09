633f1beb1f29e2aff0f5e51f119738367aa7b08f
strohli <libor@f13cybertech.com>
2021-02-09 16:36:47 +0100

    asi done

--- main.py
+++ main.py
@@ -4,24 +4,27 @@ from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.button import Button
 
 
 class MyGrid(GridLayout):
     def __init__(self, **kwargs):
         super(MyGrid,self).__init__(**kwargs)
-        self.cols = 6
-        self.add_widget(Label(text ="Name= "))
+        self.cols = 2
+        self.add_widget(Label(text ="Name"))
         self.name = TextInput(multiline=False)
         self.add_widget(self.name)
 
-        self.add_widget(Label(text="Name= "))
-        self.name = TextInput(multiline=False)
+        self.add_widget(Label(text="Password"))
+        self.password = TextInput(multiline=False)
         self.add_widget(self.name)
 
-        self.add_widget(Label(text="Name= "))
-        self.name = TextInput(multiline=False)
-        self.add_widget(self.name)
+        self.submit = Button(text="Submit", font_size=40)
+        self.submit.bind(on_press=self.pressed)
+        self.add_widget(self.submit)
 
+    def pressed(self, instance):
+        print ("test")
 
 
 class MyApp(App):
