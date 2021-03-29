edb9216c527d2e9be7e4e46399035ed1b44266ba
Libor03 <47101299+Libor03@users.noreply.github.com>
2021-03-29 16:56:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,23 @@
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
+from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.button import Button
+
+class Menu(Screen):
+    pass
+
+class Auth(Screen):
+    pass
 
 
-class MyGrid(GridLayout):
-    def __init__(self, **kwargs):
-        super(MyGrid,self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text ="Name"))
-        self.name = TextInput(multiline=False)
-        self.add_widget(self.name)
+class WindowManager(ScreenManager):
+    pass
 
-        self.add_widget(Label(text="Password"))
-        self.password = TextInput(multiline=False)
-        self.add_widget(self.name)
-
-        self.submit = Button(text="Submit", font_size=40)
-        self.submit.bind(on_press=self.pressed)
-        self.add_widget(self.submit)
-
-    def pressed(self, instance):
-        print ("test")
-
+kv = Builder.load_file("my.kv")
 
 class MyApp(App):
-
     def build(self):
-        return MyGrid()
+        return kv
 
 if __name__ == '__main__':
     MyApp().run()
+
